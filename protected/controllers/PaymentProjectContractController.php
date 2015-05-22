7cf68a51deb0069b93319966142ecf891ec8e85b
boybe7 <boybe7oo@gmail.com>
2015-05-22 18:43:04 +0700

    fix position of pagination

--- protected/controllers/PaymentProjectContractController.php
+++ protected/controllers/PaymentProjectContractController.php
@@ -31,7 +31,7 @@ class PaymentProjectContractController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','DeleteSelected'),
+				'actions'=>array('admin','delete','create','update','DeleteSelected'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
